a36c8dd78ddb45943a13e4ae5e0ef53c02e63659
snflk-su <learningsw24@gmail.com>
2024-09-21 16:42:02 -0500

    Update streamlit_app.py
    add frutivice

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,16 +35,19 @@ if ingredients_list:
 
     st.write(ingredients_string)
     
-    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     st.write(my_insert_stmt)
    
-    
     time_to_insert = st.button('Submit_Order')
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!' , icon="✅")
+        
+#New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
